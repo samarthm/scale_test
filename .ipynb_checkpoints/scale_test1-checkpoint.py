1838f9ef1f88333aa0d551583ebd720475aa9deb
Samarth Madduru <madduru@midway-login1.rcc.local>
2018-07-26 15:57:27 -0500

    first commit

---
+++ .ipynb_checkpoints/scale_test1-checkpoint.py
@@ -0,0 +1,190 @@
+import itertools
+import time
+times = []
+import parsl
+from parsl import *
+from parsl.config import Config
+from parsl.executors.ipp import IPyParallelExecutor
+from libsubmit.channels.ssh.ssh import SSHChannel
+from libsubmit.providers.slurm.slurm import SlurmProvider
+from libsubmit import *
+import subprocess
+import numpy as np
+import numpy.ma as ma
+from itertools import zip_longest
+import ipyparallel
+
+def mean(a):
+    return sum(a) / len(a)
+
+def test(x, y):
+    
+    from parsl.config import Config
+    thefile = open('times500.txt', 'w')
+
+    myNum = 0
+    ticker = 0
+    myList = []
+    num_tasks = 1000
+    print("test")
+    #parsl.set_stream_logger()
+    bigger_list = []
+    for repeat in range(0, x):
+        subprocess.call("pkill -f ipcontroller", shell=True)
+        for init_nodes_set in range(0, y):
+
+            init_workers = 2 ** init_nodes_set
+            init_tasks = init_workers
+            init_nodes = 1
+            if repeat > 5:
+                #init_tasks = 32
+                init_nodes = 2 ** (init_nodes_set - 5)
+
+            config = Config(
+                executors=[
+                    IPyParallelExecutor(
+                        provider=SlurmProvider(
+                            'broadwl',
+                            init_blocks=1,
+                            min_blocks=1,
+                            max_blocks=1,
+                            nodes_per_block=1,
+                            tasks_per_node=init_tasks,
+                            parallelism=0.5,
+                            overrides='module load Anaconda3/5.1.0; export PARSL_TESTING=True',
+                            walltime= "06:00:00"
+                        ),
+                        label='midway_ipp'
+                    )
+                ]
+            )
+            dfk = DataFlowKernel(config)
+
+
+            @App('python', dfk)
+            def sleeper(x):
+                import time
+                try:
+                    time.sleep(10)
+                except Exception as e:
+                    print("error")
+
+            client = dfk.executors['midway_ipp'].executor
+            def test_stress():
+                x = []
+                x = [sleeper(i) for i in range(0, num_tasks)]
+                start = time.time()
+                for y in x:
+                    try:
+                        y.result()
+                    except Exception as e:
+                        print("error")
+                end = time.time()
+                times.append((end-start))  
+                print('the time taken is: ' + str(end-start))
+                dfk.cleanup()
+
+
+            while int(len(client.ids)) != (init_tasks):
+                time.sleep(5)
+                print(len(client.ids))
+                print(init_tasks)
+
+            test_stress()
+
+
+
+            print(times)   
+            bigger_list.append(times)
+            thefile.write(str(times) + "\n")
+
+
+        #thefile.write(str(times) + "\n")
+
+        subprocess.call("pkill -f ipcontroller", shell=True)
+        subprocess.call("qstat -u $USER | awk '{print $1}' | grep -o [0-9]* | xargs qdel", shell=True)
+
+def test_with_data(numbers, trails, workers, nodes):
+    thefile = open('times500.txt', 'w')
+
+    myNum = 0
+    ticker = 0
+    myList = []
+    num_tasks = 1000
+    print("test")
+    #parsl.set_stream_logger()
+    bigger_list = []
+    for repeat in range(0,trials):
+        subprocess.call("pkill -f ipcontroller", shell=True)
+        for init_nodes_set in range(0,numbers):
+
+            init_workers = 2 ** init_nodes_set
+            init_tasks = init_workers
+            init_nodes = 1
+            if repeat > 5:
+                #init_tasks = 32
+                init_nodes = 2 ** (init_nodes_set - 5)
+
+            config = Config(
+                executors=[
+                    IPyParallelExecutor(
+                        provider=SlurmProvider(
+                            'broadwl',
+                            init_blocks=1,
+                            min_blocks=1,
+                            max_blocks=1,
+                            nodes_per_block=nodes,
+                            tasks_per_node=workers,
+                            parallelism=0.5,
+                            overrides='module load Anaconda3/5.1.0; export PARSL_TESTING=True',
+                            walltime= "06:00:00"
+                        ),
+                        label='midway_ipp'
+                    )
+                ]
+            )
+            dfk = DataFlowKernel(config)
+
+
+            @App('python', dfk)
+            def sleeper(x):
+                import time
+                try:
+                    time.sleep(10)
+                except Exception as e:
+                    print("error")
+
+            client = dfk.executors['midway_ipp'].executor
+            def test_stress():
+                x = []
+                x = [sleeper(i) for i in range(0, num_tasks)]
+                start = time.time()
+                for y in x:
+                    try:
+                        y.result()
+                    except Exception as e:
+                        print("error")
+                end = time.time()
+                times.append((end-start))  
+                print('the time taken is: ' + str(end-start))
+                dfk.cleanup()
+
+
+            while int(len(client.ids)) != (init_tasks):
+                time.sleep(5)
+                print(len(client.ids))
+                print(init_tasks)
+
+            test_stress()
+
+
+
+            print(times)   
+            bigger_list.append(times)
+
+
+        thefile.write(str(times) + "\n")
+        subprocess.call("pkill -f ipcontroller", shell=True)
+        subprocess.call("qstat -u $USER | awk '{print $1}' | grep -o [0-9]* | xargs qdel", shell=True)
+        
+test(6,3)
---
+++ grapher.py
@@ -0,0 +1,37 @@
+def parser(x, y):
+    
+    import matplotlib.pyplot as plt
+    import statistics 
+    
+    
+    data = open("times500.txt", "r")
+    data = data.read()
+    
+    val = 0.0
+    data = data.split(",")
+    sus = []
+    larger = []
+    for i in range(0,x):
+        for k in range(0,y):
+            print(data[i+(x-1)*k])
+            sus.append(float(data[i+(x-1)*k]))
+        larger.append(statistics.stdev(sus))
+    e = larger
+    print(e)
+
+
+    for i in range(0,x):
+        print(data[i])
+        for j in range(0,y):
+            val += float(data[i*j])
+        newarr.append((val/(x-1)))
+    print(newarr)
+    
+    return([e, newarr])
+
+def x_axis(x):
+    matrix = []
+    for i in range(0,x):
+        matrix.append(pow(2,x))
+    return matrixs
+        
